ab0f711319f41156c3706cb3f58f2fb8654760e5
SerenaHa12 <serenavander.ha@gmail.com>
2023-06-16 00:26:03 +0700

    final version 1

--- src/DigitButton.js
+++ src/DigitButton.js
@@ -1,5 +1,11 @@
-import React from 'react'
+import { ACTIONS } from "./App"
 
-export default function DigitButton({dispatch, digit}) {
-    return <button onclick = {() => dispatch}>{digit}</button>
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
 }
